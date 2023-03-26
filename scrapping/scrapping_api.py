b6735015b3f82a9bbb632a4b6346d684037a4300
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-03-26 15:40:29 -0300

    Scrapping com preenchimento em caso de NA

--- scrapping/scrapping_api.py
+++ scrapping/scrapping_api.py
@@ -10,7 +10,7 @@ import asyncio
 
 
 def request_info(date_Ymd: str) -> dict:
-    url = f"https://api.actionnetwork.com/web/v1/scoreboard/soccer?period=game&bookIds=15,30,76,75,123,69,68,972,71,247,79&date={date_Ymd}"
+    url = f"https://api.actionnetwork.com/web/v1/scoreboard/soccer?period=game&bookIds=15,30,76,75,123,69,68,972,71,247,79&date=20230325"
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
@@ -21,7 +21,7 @@ def request_info(date_Ymd: str) -> dict:
     return json.loads(response.text)
 
 
-def odds_by_time(time: datetime.datetime, names: list) -> list:
+def odds_by_time(time: datetime.datetime, names: list, ids: dict) -> list:
     response = request_info(time.strftime("%Y%m%d"))
     server_time = time + datetime.timedelta(hours=3)
     odds_all = []
@@ -44,14 +44,18 @@ def odds_by_time(time: datetime.datetime, names: list) -> list:
             else:
                 outcome = 2
 
-        odds = np.array(
-            [[odd["ml_away"], odd["ml_home"], odd["draw"]] for odd in game["odds"][2:]]
-        ).flatten()
+        odds = np.empty(len(ids) * 3)
+        odds[:] = np.nan
+        for odd in game["odds"][2:]:
+            i = ids[odd["book_id"]]
+            odds[i * 3 : i * 3 + 3] = [odd["ml_away"], odd["ml_home"], odd["draw"]]
         odds = np.append(odds, [teams[0], teams[1], outcome])
         odds_all.append(odds)
 
     odds_df = pd.DataFrame(odds_all, columns=names)
-    odds_df.to_csv(f'./scrapping/raw/{time.strftime("%Y-%m-%dT%H:%M:%S")}.csv')
+    odds_df.to_csv(
+        f'./scrapping/raw/{time.strftime("%Y-%m-%dT%H_%M_%S")}.csv', index=False
+    )
 
     return odds_df
 
@@ -69,7 +73,7 @@ def datetimes_all(response: dict) -> list:
 
 
 def main():
-    BETTING_SITES = [
+    BETTING_BOOKS = [
         "DraftKings NJ Sportsbook",
         "FanDuel NJ Sportsbook",
         "BetRivers NJ Sportsbook (1)",
@@ -80,10 +84,12 @@ def main():
         "UnibetNJ Sportsbook",
         "BetRivers NY Sportsbook",
     ]
+    BOOK_ID = [68, 69, 71, 75, 76, 79, 123, 247, 972]
+    BOOK_ID_ORDER = dict(zip(BOOK_ID, list(range(len(BOOK_ID)))))
     OUTCOMES = ["Away", "Home", "Draw"]
 
     NAMES_ZIP = zip(
-        np.repeat(BETTING_SITES, len(OUTCOMES)), np.tile(OUTCOMES, len(BETTING_SITES))
+        np.repeat(BETTING_BOOKS, len(OUTCOMES)), np.tile(OUTCOMES, len(BETTING_BOOKS))
     )
     NAMES_CONCAT = list(map(lambda x: f"{x[0]} | {x[1]}", NAMES_ZIP))
     NAMES_CONCAT.append("Away")
@@ -91,24 +97,24 @@ def main():
     NAMES_CONCAT.append("Outcome")
 
     today = datetime.datetime.now().strftime("%Y%m%d")
-    print(datetime.datetime.now().strftime("%Y/%m/%d_%H:%M:%S"))
     response = request_info(today)
     date_times = datetimes_all(response)
-    odds_by_time(date_times[0], NAMES_CONCAT)
 
-    # scheduler = AsyncIOScheduler()
+    scheduler = AsyncIOScheduler()
 
-    # for dt in date_times:
-    #     print(dt)
-    #     scheduler.add_job(odds_by_time, "date", run_date=dt, args=(dt, NAMES_CONCAT))
-    #     break
+    for dt in date_times:
+        print(dt)
+        scheduler.add_job(
+            odds_by_time, "date", run_date=dt, args=(dt, NAMES_CONCAT, BOOK_ID_ORDER)
+        )
+        break
 
-    # scheduler.start()
-    # try:
-    #     asyncio.get_event_loop().run_forever()
-    # except (KeyboardInterrupt, SystemExit):
-    #     pass
-    #     scheduler.shutdown(wait=False)
+    scheduler.start()
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+        scheduler.shutdown(wait=False)
 
 
 if __name__ == "__main__":
