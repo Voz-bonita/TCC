74e2c8b53566826fbffbef5adad581a78c4be147
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-07-05 20:42:36 -0300

    Correlation tests

--- exploratory_info.r
+++ exploratory_info.r
@@ -1,7 +1,16 @@
-pacman::p_load("jsonlite", "dplyr", "ggplot2")
+pacman::p_load("jsonlite", "dplyr", "ggplot2", "purrr")
 
 data <- read_json("./scrapping/info_all.json")
 source("extra_functions.r", encoding = "UTF-8")
+
+expectation <- function(variable, vals, df, moment = 1) {
+    expect_variable <- (vals^moment * map_dbl(vals, ~
+        filter_at(df, vars(variable), any_vars(. == .x)) %>%
+            pull(Proporção) %>%
+            sum())) %>%
+        sum()
+    return(expect_variable)
+}
 
 x <- 0:5
 y <- 0:5
@@ -9,8 +18,11 @@ size <- length(x) * length(y)
 score_df <- expand.grid(x, y) %>%
     cbind(., rep(0, size)) %>%
     cbind(., rep(names(data), each = size)) %>%
-    rename_all(~ c("x", "y", "Proporção", "league"))
-league_example <- names(data)[1]
+    rename_all(~ c("x", "y", "Proporção", "liga"))
+corr_test_df <- data.frame(
+    "Liga" = c(NA), "n" = c(NA), "r" = c(NA),
+    "Estatística" = c(NA), "p-valor" = c(NA)
+)
 
 for (league in names(data)) {
     league_index <- which(names(data) == league)
@@ -22,11 +34,29 @@ for (league in names(data)) {
         index <- (home + 6 * away + 1) + (league_index - 1) * size
         score_df[index, "Proporção"] <- score_df[index, "Proporção"] + 1 / n_max
     }
+
+    score_xy <- filter(score_df, liga == league) %>%
+        mutate("xy" = x * y)
+    all_xy <- unique(score_xy[["xy"]])
+    exy <- expectation(variable = "xy", vals = all_xy, df = score_xy)
+    ex <- expectation(variable = "x", vals = x, df = score_xy)
+    ex2 <- expectation(variable = "x", vals = x, df = score_xy, moment = 2)
+    ey <- expectation(variable = "y", vals = y, df = score_xy)
+    ey2 <- expectation(variable = "y", vals = y, df = score_xy, moment = 2)
+    varx <- ex2 - ex^2
+    vary <- ey2 - ey^2
+    corr <- (exy - ex * ey) / sqrt(varx) / sqrt(vary)
+    tstar <- corr * sqrt(n_max - 2) / sqrt(1 - corr^2)
+    out_vec <- c(
+        "Liga" = league, "n" = n_max, "corr" = corr, "Estística" = tstar,
+        "p-valor" = 2 * pt(abs(tstar), df = n_max - 2, lower.tail = F)
+    )
+    corr_test_df <- corr_test_df %>% rbind(out_vec)
 }
 
 (ggplot(data = score_df, aes(x = `x`, y = `y`, fill = `Proporção`)) +
     geom_tile(color = "black", lwd = 0.5) +
-    facet_wrap(vars(league)) +
+    facet_wrap(vars(liga)) +
     xlab("Gols do time da casa") +
     ylab("Gols to time visitante") +
     scale_x_continuous(breaks = 0:5) +
